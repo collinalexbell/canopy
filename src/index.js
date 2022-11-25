c36113b018d9fbc9e25c589e03e0a67b79e64a25
kuberlog <collinalexbell@gmail.com>
2022-11-25 07:50:27 +0000

    extract fn to file

--- src/index.js
+++ src/index.js
@@ -5,29 +5,7 @@ window.onload = () => {
 
   // Add your code here matching the playground format
 
-  const createScene = () => {
-    const scene = new BABYLON.Scene(engine);
-
-    const camera = new BABYLON.ArcRotateCamera(
-      "camera",
-      -Math.PI / 2,
-      Math.PI / 2.5,
-      3,
-      new BABYLON.Vector3(0, 0, 0)
-    );
-    camera.attachControl(canvas, true);
-
-    const light = new BABYLON.HemisphericLight(
-      "light",
-      new BABYLON.Vector3(0, 1, 0)
-    );
-
-    const box = BABYLON.MeshBuilder.CreateBox("box", {});
-
-    return scene;
-  };
-
-  const scene = createScene(); //Call the createScene function
+  const scene = createScene(engine, canvas); //Call the createScene function
 
   // Register a render loop to repeatedly render the scene
   engine.runRenderLoop(function () {
