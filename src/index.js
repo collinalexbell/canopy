476f83bf9bef42d4675d9eb2e35f55c8da399a61
kuberlog <collinalexbell@gmail.com>
2022-11-25 07:42:33 +0000

    now it is a box

--- src/index.js
+++ src/index.js
@@ -1,17 +1,41 @@
-const canvas = document.getElementById("renderCanvas"); // Get the canvas element
-const engine = new BABYLON.Engine(canvas, true); // Generate the BABYLON 3D engine
+window.onload = () => {
+  console.log("init");
+  const canvas = document.getElementById("renderCanvas"); // Get the canvas element
+  const engine = new BABYLON.Engine(canvas, true); // Generate the BABYLON 3D engine
 
-// Add your code here matching the playground format
+  // Add your code here matching the playground format
 
-const scene = createScene(); //Call the createScene function
+  const createScene = () => {
+    const scene = new BABYLON.Scene(engine);
 
-// Register a render loop to repeatedly render the scene
-engine.runRenderLoop(function () {
-  scene.render();
-});
+    const camera = new BABYLON.ArcRotateCamera(
+      "camera",
+      -Math.PI / 2,
+      Math.PI / 2.5,
+      3,
+      new BABYLON.Vector3(0, 0, 0)
+    );
+    camera.attachControl(canvas, true);
 
-// Watch for browser/canvas resize events
-window.addEventListener("resize", function () {
-  engine.resize();
-});
+    const light = new BABYLON.HemisphericLight(
+      "light",
+      new BABYLON.Vector3(0, 1, 0)
+    );
 
+    const box = BABYLON.MeshBuilder.CreateBox("box", {});
+
+    return scene;
+  };
+
+  const scene = createScene(); //Call the createScene function
+
+  // Register a render loop to repeatedly render the scene
+  engine.runRenderLoop(function () {
+    scene.render();
+  });
+
+  // Watch for browser/canvas resize events
+  window.addEventListener("resize", function () {
+    engine.resize();
+  });
+}
